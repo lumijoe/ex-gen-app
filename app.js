9361f200dff91dd30586840186028d25d76ef16c
lumijoe <spacedesign63@gmail.com>
2023-11-21 15:01:26 +0900

    page195

--- app.js
+++ app.js
@@ -1,41 +1,52 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// app.js
+// list4-4(p194)1121
 
+// 1:要モジュールのロード
+var createError = require('http-errors');        // error（エラー対処）
+var express = require('express');                // express（本体）
+var path = require('path');                      // path（ファイルパスを扱う）
+var cookieParser = require('cookie-parser');     // cookie-parser（値変換処理系）
+var logger = require('morgan');                  // logger morgan（reqログ出力系）
+
+// 2:ルート用モジュールのロード
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// 3:Expressオブジェクトの作成と基本設定
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware setup
+app.use(logger('dev'));                                  // logger dev
+app.use(express.json());                                 // express.json
+app.use(express.urlencoded({ extend: false }));          // express.urlencoded
+app.use(cookieParser());                                 // cookieParser
+app.use(express.static(path.join(__dirname, 'public'))); // express.static path.json
 
+// 4:app.useによる関数の組み込み
+// Serve routes using routers
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('users', usersRouter);
 
-// catch 404 and forward to error handler
+// 5:アクセスするルートとエラー用のapp.use作成
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+// 6:module.expressの設定
+module.express = app;
