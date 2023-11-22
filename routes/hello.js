725a41b63fd09cc4d04d72407a687718fa248612
lumijoe <spacedesign63@gmail.com>
2023-11-22 18:25:50 +0900

    page228

--- routes/hello.js
+++ routes/hello.js
@@ -1,33 +1,37 @@
 // routes/hello.js
-// list4-16(p217)1122
+// list4-17(p221)1122
 // views/hello.ejsにアクセスされた時の処理をするhello.js
 // app.jsで使用できるようにexport
 
 const express = require('express');
 const router = express.Router();
+const http = require('https');
+const parseString = require('xml2js').parseString;
+
 
 // http://localhost:3000/hello　へアクセス
 router.get('/', (req, res, next) => {
-    var msg = '※ 何か書いて送信してください。';
-    if (req.session.message != undefined) {
-        msg = `Last Message: ${ req.session.message }`;
-    }
-
-    var data = {
-        title: 'Hello!',
-        content: msg
+    var opt = {
+        host: 'news.google.com',
+        port: 443,
+        path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja'
     };
-    res.render('hello', data);
-});
-
-router.post('/post', (req, res, next) => {
-    var msg = req.body['message'];
-    req.session.message = msg;
-    var data = {
-        title: 'Hello!',
-        content: `Last Message: ${ req.session.message }`
-    };
-    res.render('hello', data);
+    http.get(opt, (res2) => {
+        var body = '';
+        res2.on('data', (data) => {
+            body += data;
+        });
+        res2.on('end', () => {
+            parseString(body.trim(), (err, result) => {
+                console.log(result);
+                var data = {
+                    title: 'Google News',
+                    content: result.rss.channel[0].item
+                };
+                res.render('hello', data);
+            });
+        })
+    });
 });
 
 module.exports = router;
