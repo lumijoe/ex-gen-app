ccbce73481db1344926bf5962b6c3d03bde3f2dc
lumijoe <spacedesign63@gmail.com>
2023-11-22 15:24:35 +0900

    page212

--- routes/hello.js
+++ routes/hello.js
@@ -1,17 +1,25 @@
 // routes/hello.js
-// list4-11(p208)1121
+// list4-14(p211)1122
 // views/hello.ejsにアクセスされた時の処理をするhello.js
 // app.jsで使用できるようにexport
 
 const express = require('express');
 const router = express.Router();
-// アドレス入力：　http://localhost:3000/hello?name=hanako&mail=hanako@flower.com
+
+// http://localhost:3000/hello　へアクセス
 router.get('/', (req, res, next) => {
-    var name = req.query.name;
-    var mail = req.query.mail;
     var data = {
         title: 'Hello!',
-        content: 'あなたの名前は、' + name + '。<br>' + 'メールアドレスは、' + mail + 'です。'
+        content: '※ 何か書いて送信してください。'
+    };
+    res.render('hello', data);
+});
+
+router.post('/post', (req, res, next) => {
+    var msg = req.body['message'];
+    var data = {
+        title: 'Hello!',
+        content: `あなたは、「 ${msg} 」と送信しました。`
     };
     res.render('hello', data);
 });
