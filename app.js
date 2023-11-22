6a8f920c9a4460b716219282704c4848b4c128c2
lumijoe <spacedesign63@gmail.com>
2023-11-22 17:44:11 +0900

    page220

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 // app.js
 // list4-15(p215)1122
 
-// 1:要モジュールのロード
+// 1:モジュールのrequire
 var createError = require('http-errors');        // error（エラー対処）
 var express = require('express');                // express（本体）
 var path = require('path');                      // path（ファイルパスを扱う）
 var cookieParser = require('cookie-parser');     // cookie-parser（値変換処理系）
 var logger = require('morgan');                  // logger morgan（reqログ出力系）
+const session = require('express-session');      // express-session
 
-// 2:ルート用モジュールのロード
+// 2:ルーティング設定
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var hello = require('./routes/hello');
@@ -28,7 +29,16 @@ app.use(express.urlencoded({ extend: false }));          // express.urlencoded
 app.use(cookieParser());                                 // cookieParser
 app.use(express.static(path.join(__dirname, 'public'))); // express.static path.json
 
-// 4:app.useによる関数の組み込み
+// express-session set up
+var session_opt = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 1000 }
+};
+app.use(session(session_opt));
+
+// 4:ルーティング使用
 // Serve routes using routers
 app.use('/', indexRouter);
 app.use('users', usersRouter);
--- routes/hello.js
+++ routes/hello.js
@@ -1,5 +1,5 @@
 // routes/hello.js
-// list4-14(p211)1122
+// list4-16(p217)1122
 // views/hello.ejsにアクセスされた時の処理をするhello.js
 // app.jsで使用できるようにexport
 
@@ -8,18 +8,24 @@ const router = express.Router();
 
 // http://localhost:3000/hello　へアクセス
 router.get('/', (req, res, next) => {
+    var msg = '※ 何か書いて送信してください。';
+    if (req.session.message != undefined) {
+        msg = `Last Message: ${ req.session.message }`;
+    }
+
     var data = {
         title: 'Hello!',
-        content: '※ 何か書いて送信してください。'
+        content: msg
     };
     res.render('hello', data);
 });
 
 router.post('/post', (req, res, next) => {
     var msg = req.body['message'];
+    req.session.message = msg;
     var data = {
         title: 'Hello!',
-        content: `あなたは、「 ${msg} 」と送信しました。`
+        content: `Last Message: ${ req.session.message }`
     };
     res.render('hello', data);
 });
