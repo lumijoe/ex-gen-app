52ce1e613683915f5faa4de6ddd7d5a89f75f0d3
lumijoe <spacedesign63@gmail.com>
2023-11-24 15:59:32 +0900

    add sqlite3

--- routes/hello.js
+++ routes/hello.js
@@ -1,38 +1,35 @@
 // routes/hello.js
-// list4-17(p221)1122
+// list5-2(p249)1124
 // views/hello.ejsにアクセスされた時の処理をするhello.js
 // app.jsで使用できるようにexport
 
 const express = require('express');
 const router = express.Router();
-const http = require('https');
-const parseString = require('xml2js').parseString;
+const sqlite3 = require('sqlite3'); //　追加
 
+// データベースオブジェクトの取得
+const db = new sqlite3.Database('mydb.sqlite3');
 
-// http://localhost:3000/hello　へアクセス
-// router.get('/', (req, res, next) => {
-//     var opt = {
-//         host: 'news.google.com',
-//         port: 443,
-//         path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja'
-//     };
-//     http.get(opt, (res2) => {
-//         var body = '';
-//         res2.on('data', (data) => {
-//             body += data;
-//         });
-//         res2.on('end', () => {
-//             parseString(body.trim(), (err, result) => {
-//                 console.log(result);
-//                 var data = {
-//                     title: 'Google News',
-//                     content: result.rss.channel[0].item
-//                 };
-//                 res.render('hello', data);
-//             });
-//         })
-//     });
-// });
+// GETアクセスの処理
+router.get('/', (req, res, next) => {
+
+    // データベースのシリアライズ
+    db.serialize(() => {
+        // レコードを全て取り出す
+        db.all("select * from mydata",(err, rows) => {
+            // データベースアクセス完了時の処理
+            if (!err) {
+                var data = {
+                    title: 'Hello!',
+                    content: rows // 取得したレコードデータ
+                };
+                res.render('hello', data);
+            }
+        });
+    });
+});
 
 module.exports = router;
+// http://localhost:3000/hello
+
 
