6155c85a14940fff5afa1e8664f4117160a332aa
lumijoe <spacedesign63@gmail.com>
2023-11-27 16:48:14 +0900

    page277

--- routes/hello.js
+++ routes/hello.js
@@ -97,6 +97,33 @@ router.post('/edit', (req, res, next) => {
     res.redirect('/hello');
 });
 
+//　delete(delete.ejs)の処理を作成
+router.get('/delete', (req, res, next) => {
+    const id = req.query.id;
+    db.serialize(() => {
+        const q = "select * from mydata where id = ?";
+        db.get(q, [id], (err, row) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello/Delete',
+                    content: `id = ${id} のレコードを削除：`,
+                    mydata: row
+                }
+                res.render('hello/delete', data);
+            }
+        });
+    });
+});
+
+router.post('/delete', (req, res, next) => {
+    const id = req.body.id;
+    db.serialize(() => {
+        const q = "delete from mydata where id = ?";
+        db.run(q, id);
+    });
+    res.redirect('/hello');
+});
+
 module.exports = router;
 // http://localhost:3000/hello
 
