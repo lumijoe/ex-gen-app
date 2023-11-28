b5c498c53fcbfdcdb791985adc08f90c6fba0196
lumijoe <spacedesign63@gmail.com>
2023-11-28 11:17:49 +0900

    page281

--- routes/hello.js
+++ routes/hello.js
@@ -125,6 +125,41 @@ router.post('/delete', (req, res, next) => {
     res.redirect('/hello');
 });
 
+// find(find.ejs)の処理を作成
+router.get('find', (req, res, next) => {
+    db.selialize(() => {
+        db.all("select * from mydata", (err, rows) => {
+            if (!err) {
+                var data = {
+                    title: 'hello/find',
+                    find: '',
+                    content: '検索条件を入力してください',
+                    mydata: rows
+                };
+                res.render('hello/find', data);
+            }
+        });
+    });
+});
+
+router.post('find', (req, res, next) => {
+    var find = req.body.find;
+    db.selialize(() => {
+        var q = "select * from mydata where ";
+        db.all(q + find, [], (err, rows) => {
+            if (!err) {
+                var data = {
+                    title: 'Hello/find',
+                    find:find,
+                    content: `検索条件 ${find}`,
+                    mydata: rows
+                };
+                res.render('hello/find', data);
+            }
+        });
+    });
+});
+
 module.exports = router;
 // http://localhost:3000/hello
 
